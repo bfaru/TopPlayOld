1f4f2bb6bb3f6a05f9814888534c98bbce0c0a20
Bryan Garces <bryangarces@Bryans-MacBook-Air.local>
2013-10-13 20:45:50 -0400

    Initial Commit

---
+++ TopPlay/TopPlayViewController.m
@@ -0,0 +1,29 @@
+//
+//  TopPlayViewController.m
+//  TopPlay
+//
+//  Created by Bryan Garces on 10/13/13.
+//  Copyright (c) 2013 Bryan Garces. All rights reserved.
+//
+
+#import "TopPlayViewController.h"
+
+@interface TopPlayViewController ()
+
+@end
+
+@implementation TopPlayViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TopPlay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TopPlay
+//
+//  Created by Bryan Garces on 10/13/13.
+//  Copyright (c) 2013 Bryan Garces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TopPlayAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TopPlayAppDelegate class]));
+    }
+}
