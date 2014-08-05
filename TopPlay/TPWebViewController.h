eece9c2d713bfb3a3fc3670e9becca21843e328f
Bryan Garces <bryangtech@gmail.com>
2014-08-05 12:24:50 -0400

    first stage of webView controller

---
+++ TopPlay/TPWebViewController.h
@@ -0,0 +1,14 @@
+//
+//  TPWebViewController.h
+//  TopPlay
+//
+//  Created by Bryan Garces on 8/5/14.
+//  Copyright (c) 2014 Bryan Garces. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPWebViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
